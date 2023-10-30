80b664b868afd4a0eae05af691bb90e576b8f9c2
Cédric HT <altaris@users.noreply.github.com>
2023-10-30 19:03:05 +0900

    Merged VHTorchvisionClassifier into Classifier

--- nlnas/classifier.py
+++ nlnas/classifier.py
@@ -1,17 +1,19 @@
 """A torchvision image classifier wrapped inside a `LightningModule`"""
 
-from itertools import chain
+# from itertools import chain
 from pathlib import Path
-from typing import Any, Iterable, Literal, Tuple
+from typing import Any, Iterable, Literal
 
 import pytorch_lightning as pl
 import torch
 import torchmetrics
 from torch import Tensor, nn
-from torch.optim import Optimizer
+
+# from torch.optim import Optimizer
 from torch.utils.hooks import RemovableHandle
 from torchvision.models import get_model
-from tqdm import tqdm
+
+# from tqdm import tqdm
 
 from .separability import gdv, label_variation
 from .utils import best_device
@@ -21,18 +23,78 @@ class Classifier(pl.LightningModule):
     """Classifier model with some extra features"""
 
     n_classes: int
+    sep_submodules: list[str]
+    sep_score: Literal["gdv", "lv"]
+    sep_weight: float
 
-    def __init__(self, n_classes: int, **kwargs: Any) -> None:
+    def __init__(
+        self,
+        n_classes: int,
+        sep_submodules: list[str] | None = None,
+        sep_score: Literal["gdv", "lv"] = "gdv",
+        sep_weight: float = 1e-1,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Args:
+            n_classes (int):
+            sep_submodules (list[str] | None, optional): Submodules to consider
+                for the latent separation score
+            sep_score (Literal[&quot;gdv&quot;, &quot;lv&quot;], optional):
+                Type of separation score, either `"gdv"` (see
+                `nlnas.separability.gdv`) or `"lv"` (see
+                `nlnas.separability.lv`). Ignored if `sep_submodules` is left
+                to `None`
+            sep_weight (float, optional): Weight of the separation score.
+                Ignored if `sep_submodules` is left to `None`
+        """
         super().__init__(**kwargs)
+        self.save_hyperparameters()
         self.n_classes = n_classes
+        self.sep_submodules = sep_submodules or []
+        self.sep_score, self.sep_weight = sep_score, sep_weight
 
     def _evaluate(self, batch, stage: str | None = None) -> Tensor:
         """Self-explanatory"""
         x, y = batch
-        logits = self(x)
+        y = y.to(self.device)
+
+        output_dict: dict[str, Tensor] = {}
+        logits = self.forward_intermediate(
+            x, self.sep_submodules, output_dict, keep_gradients=True
+        )
         loss = nn.functional.cross_entropy(logits, y.long())
+
+        if self.sep_submodules:
+            if self.sep_score == "gdv":
+                sep_loss = torch.stack(
+                    [gdv(v, y) for v in output_dict.values()]
+                ).mean()
+            elif self.sep_score == "lv":
+                sep_loss = torch.stack(
+                    [
+                        label_variation(
+                            v, y.to(v), k=10, n_classes=self.n_classes
+                        )
+                        for v in output_dict.values()
+                    ]
+                ).mean()
+            else:
+                raise RuntimeError(
+                    f"Unknown separation score type '{self.sep_score}'."
+                )
+        else:
+            sep_loss = torch.tensor(0.0)
+
         if stage:
             self.log(f"{stage}/loss", loss, prog_bar=True, sync_dist=True)
+        if stage == "train" and self.sep_submodules:
+            self.log(
+                f"{stage}/{self.sep_score}",
+                sep_loss,
+                prog_bar=True,
+                sync_dist=True,
+            )
         if stage and best_device() != "mps":
             # NotImplementedError: The operator 'aten::_unique2' is not
             # currently implemented for the MPS device. If you want this op to
@@ -51,16 +113,10 @@ class Classifier(pl.LightningModule):
                 top_k=1,
             )
             self.log(f"{stage}/acc", acc, prog_bar=True, sync_dist=True)
-        return loss
+        return loss + self.sep_weight * sep_loss
 
     def configure_optimizers(self):
-        # return torch.optim.Adam(self.parameters())
-        return torch.optim.SGD(
-            self.parameters(),
-            lr=5e-2,
-            momentum=0.9,
-            weight_decay=5e-4,
-        )
+        return torch.optim.Adam(self.parameters())
 
     def forward_intermediate(
         self,
@@ -215,144 +271,3 @@ class TruncatedClassifier(Classifier):
     def forward(self, x: Tensor, *_, **__) -> Tensor:
         self.model(x.to(self.device))  #  type: ignore
         return self.fc(self._out.flatten(1))
-
-
-class VHTorchvisionClassifier(TorchvisionClassifier):
-    """
-    Torchvision classifier with a horizontal loss optimization in addition
-    to the usual cross-entropy optimization
-    """
-
-    # pylint: disable=unused-argument
-    def __init__(
-        self,
-        model_name: str,
-        n_classes: int,
-        submodules: list[str],
-        input_shape: Iterable[int] | None = None,
-        model_config: dict[str, Any] | None = None,
-        add_final_fc: bool = False,
-        separation_score: Literal["gdv", "lv"] | None = None,
-        separation_weight: float = 1e-1,
-        **kwargs,
-    ) -> None:
-        """
-        Args:
-            model_name (str): See `nlnas.classifier.TorchvisionClassifier`
-            n_classes (int): See `nlnas.classifier.TorchvisionClassifier`
-            submodules (list[str]): Names of the submodules that are to be
-                trained horizontally (as well as vertically)
-            input_shape (Iterable[int] | None, optional): See
-                `nlnas.classifier.TorchvisionClassifier`
-            model_config (dict[str, Any] | None, optional): See
-                `nlnas.classifier.TorchvisionClassifier`
-            add_final_fc (bool, optional): See
-                `nlnas.classifier.TorchvisionClassifier`
-            separation_score (either `"gdv"`, `"lv"`, or `None`):
-            separation_weight (float, optional):
-        """
-        super().__init__(
-            model_name,
-            n_classes,
-            input_shape,
-            model_config,
-            add_final_fc,
-            **kwargs,
-        )
-        self.save_hyperparameters()
-        # self.automatic_optimization = False
-
-    def _evaluate(self, batch, stage: str | None = None) -> Tensor:
-        """Self-explanatory"""
-        x, y = batch
-        y = y.to(self.device)
-        output_dict: dict[str, Tensor] = {}
-        logits = self.forward_intermediate(
-            x, self.hparams["submodules"], output_dict, keep_gradients=True
-        )
-        v_loss = nn.functional.cross_entropy(logits, y.long())
-        separation_score: str | None = self.hparams["separation_score"]
-        if separation_score == "gdv":
-            h_loss = torch.stack(
-                [gdv(v, y) for v in output_dict.values()]
-            ).mean()
-        elif separation_score == "lv":
-            h_loss = torch.stack(
-                [
-                    label_variation(v, y.to(v), k=10, n_classes=self.n_classes)
-                    for v in output_dict.values()
-                ]
-            ).mean()
-        else:
-            h_loss = torch.tensor(0.0)
-        if stage:
-            self.log(f"{stage}/loss", v_loss, prog_bar=True, sync_dist=True)
-        if stage == "train" and separation_score is not None:
-            self.log(
-                f"{stage}/{separation_score}",
-                h_loss,
-                prog_bar=True,
-                sync_dist=True,
-            )
-        if stage and best_device() != "mps":
-            # NotImplementedError: The operator 'aten::_unique2' is not
-            # currently implemented for the MPS device. If you want this op to
-            # be added in priority during the prototype phase of this feature,
-            # please comment on
-            # https://github.com/pytorch/pytorch/issues/77764. As a temporary
-            # fix, you can set the environment variable
-            # `PYTORCH_ENABLE_MPS_FALLBACK=1` to use the CPU as a fallback for
-            # this op. WARNING: this will be slower than running natively on
-            # MPS.
-            acc = torchmetrics.functional.accuracy(
-                torch.argmax(logits, dim=1),
-                y,
-                "multiclass",
-                num_classes=self.n_classes,
-                top_k=1,
-            )
-            self.log(f"{stage}/acc", acc, prog_bar=True, sync_dist=True)
-        return v_loss + self.hparams["separation_weight"] * h_loss
-
-    # def configure_optimizers(self):
-    #     v_opt = torch.optim.Adam(self.parameters(), lr=1e-3)
-    #     h_opt = torch.optim.Adam(
-    #         chain(
-    #             *[
-    #                 self.get_submodule(s).parameters()
-    #                 for s in self.hparams["submodules"]
-    #             ]
-    #         ),
-    #         lr=1e-3,
-    #     )
-    #     return [v_opt, h_opt]
-
-    # pylint: disable=arguments-differ
-    # def training_step(self, batch: Any, *_: Any, **__: Any) -> Tensor:
-    #     v_opt, _ = self.optimizers()  # type: ignore  # pylint: disable=unpacking-non-sequence
-    #     assert isinstance(v_opt, Optimizer)
-    #     # assert isinstance(h_opt, Optimizer)
-    #     v_loss, _ = self._evaluate_vh(batch, "train")
-    #     v_opt.zero_grad()
-    #     self.manual_backward(v_loss)
-    #     v_opt.step()
-    #     # h_opt.zero_grad()
-    #     # self.manual_backward(h_loss)
-    #     # h_opt.step()
-    #     return v_loss
-
-    # def on_train_epoch_end(self) -> None:
-    #     dl = self.trainer.train_dataloader
-    #     if dl is None:
-    #         raise RuntimeError(
-    #             "Model's trainer does not have a training dataloader. "
-    #             "This should really not happen =("
-    #         )
-    #     _, h_opt = self.optimizers()  # type: ignore
-    #     assert isinstance(h_opt, Optimizer)
-    #     progress = tqdm(iter(dl), desc="Horizontal fit", leave=False)
-    #     for batch in progress:
-    #         h_opt.zero_grad()  # type: ignore
-    #         _, h_loss = self._evaluate(batch, "train")
-    #         self.manual_backward(h_loss)
-    #         h_opt.step()
