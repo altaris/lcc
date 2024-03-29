3564b449057ae0e83ea3b5ed538ee9fa47e8c7ef
Cédric HT <altaris@users.noreply.github.com>
2024-03-29 11:32:19 +0900

    Pretty print module and submodules

--- nlnas/utils.py
+++ nlnas/utils.py
@@ -3,6 +3,7 @@
 import os
 
 import torch
+from torch import nn
 
 
 def best_device() -> str:
@@ -17,3 +18,56 @@ def best_device() -> str:
             else "mps" if torch.backends.mps.is_available() else "cpu"
         )
     return accelerator
+
+
+def pretty_print_submodules(
+    module: nn.Module,
+    exclude_non_trainable: bool = False,
+    max_depth: int | None = None,
+    prefix: str = "",
+    current_depth: int = 0,
+):
+    """
+    Recursively prints a module and its submodule in a hierarchical manner.
+
+        >>> pretty_print_submodules(model, max_depth=4)
+        model -> ResNetForImageClassification
+        |-----resnet -> ResNetModel
+        |     |------embedder -> ResNetEmbeddings
+        |     |      |--------embedder -> ResNetConvLayer
+        |     |      |        |--------convolution -> Conv2d
+        |     |      |        |--------normalization -> BatchNorm2d
+        |     |      |        |--------activation -> ReLU
+        |     |      |--------pooler -> MaxPool2d
+        |     |------encoder -> ResNetEncoder
+        |     |      |-------stages -> ModuleList
+        |     |      |       |------0 -> ResNetStage
+        |     |      |       |------1 -> ResNetStage
+        |     |      |       |------2 -> ResNetStage
+        |     |      |       |------3 -> ResNetStage
+        |     |------pooler -> AdaptiveAvgPool2d
+        |-----classifier -> Sequential
+        |     |----------0 -> Flatten
+        |     |----------1 -> Linear
+
+    Args:
+        module (nn.Module):
+        exclude_non_trainable (bool, optional):
+        max_depth (int | None, optional):
+        prefix (str, optional): Don't use
+        current_depth (int, optional): Don't use
+    """
+    if max_depth is not None and current_depth > max_depth:
+        return
+    for k, v in module.named_children():
+        if exclude_non_trainable and len(list(v.parameters())) == 0:
+            continue
+        print(prefix + k, "->", v.__class__.__name__)
+        p = prefix.replace("-", " ") + "|" + ("-" * len(k))
+        pretty_print_submodules(
+            module=v,
+            exclude_non_trainable=exclude_non_trainable,
+            max_depth=max_depth,
+            prefix=p,
+            current_depth=current_depth + 1,
+        )
