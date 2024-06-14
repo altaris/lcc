39f4c63fa79a6e5b2f8d1f61ec74301c9a29a822
Cédric HT <altaris@users.noreply.github.com>
2024-06-14 12:44:09 +0900

    Save/load batched tensors

--- nlnas/utils.py
+++ nlnas/utils.py
@@ -1,9 +1,13 @@
 """Useful stuff"""
 
 import os
+from pathlib import Path
+from typing import Any, Callable, Literal
 
+import numpy as np
 import torch
-from torch import nn
+from safetensors import torch as st
+from torch import Tensor, nn
 
 
 def best_device() -> str:
@@ -18,6 +22,48 @@ def best_device() -> str:
             else "mps" if torch.backends.mps.is_available() else "cpu"
         )
     return accelerator
+
+
+def load_tensor_batched(
+    output_dir: str | Path,
+    prefix: str = "batch",
+    extension: str = "st",
+    tqdm_style: Literal["notebook", "console", "none"] | None = None,
+):
+    """Inverse of `save_tensor_batched`."""
+    t = make_tqdm(tqdm_style)
+    return torch.concat(
+        [
+            st.load_file(p)[""]
+            for p in t(
+                sorted(Path(output_dir).glob(f"{prefix}.*.{extension}")),
+                "Loading",
+                leave=False,
+            )
+        ]
+    )
+
+
+def make_tqdm(
+    style: Literal["notebook", "console", "none"] | None = "console"
+) -> Callable:
+    """Returns the appropriate tqdm factory function based on the style"""
+
+    def _fake_tqdm(x: Any, *args, **kwargs):  # pylint: disable=unused-argument
+        return x
+
+    if style is None or style == "none":
+        f = _fake_tqdm
+    elif style == "console":
+        from tqdm import tqdm as f  # type: ignore
+    elif style == "notebook":
+        from tqdm.notebook import tqdm as f  # type: ignore
+    else:
+        raise ValueError(
+            f"Unknown TQDM style '{style}'. Available styles are 'notebook', "
+            "'console', or None"
+        )
+    return f
 
 
 def pretty_print_submodules(
@@ -70,4 +116,35 @@ def pretty_print_submodules(
             max_depth=max_depth,
             prefix=p,
             current_depth=current_depth + 1,
+        )
+
+
+def save_tensor_batched(
+    x: Tensor | np.ndarray,
+    output_dir: str | Path,
+    prefix: str = "batch",
+    batch_size: int = 256,
+    extension: str = "st",
+    tqdm_style: Literal["notebook", "console", "none"] | None = None,
+) -> None:
+    """
+    Saves a tensor in batches of `batch_size` elements. The files will be named
+    `output_dir/<prefix>.<batch_idx>.<extension>`. The batches are saved using
+    safetensors.
+
+    It would be great if you could adjust the batch size so that there are less
+    than 10000 batches :]
+
+    Args:
+        x (Tensor):
+        output_dir (str):
+        prefix (str, optional):
+        batch_size (int, optional):
+        extension (str, optional):
+    """
+    batches = (Tensor(x) if isinstance(x, np.ndarray) else x).split(batch_size)
+    t = make_tqdm(tqdm_style)
+    for i, batch in enumerate(t(batches, "Saving", leave=False)):
+        st.save_file(
+            {"": batch}, Path(output_dir) / f"{prefix}.{i:04}.{extension}"
         )
