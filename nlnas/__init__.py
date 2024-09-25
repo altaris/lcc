196b76c9c903fbfeee48de52ad00db457ce8c304
Cédric HT <altaris@users.noreply.github.com>
2024-09-25 16:23:34 +0900

    Reduced imports overhead

--- nlnas/__init__.py
+++ nlnas/__init__.py
@@ -2,32 +2,3 @@
 .. include:: ../README.md
 .. include:: ../CHANGELOG.md
 """
-
-from .analysis import analyse_ckpt, analyse_training
-from .classifiers import (
-    BaseClassifier,
-    HuggingFaceClassifier,
-    TimmClassifier,
-    TorchvisionClassifier,
-    WrappedClassifier,
-    full_dataset_latent_clustering,
-)
-from .correction import (
-    class_otm_matching,
-    lcc_knn_indices,
-    lcc_loss,
-    lcc_targets,
-    louvain_communities,
-    max_connected_confusion_choice,
-    otm_matching_predicates,
-)
-from .datasets import (
-    DEFAULT_DATALOADER_KWARGS,
-    HuggingFaceDataset,
-    TorchvisionDataset,
-    WrappedDataset,
-    dl_head,
-)
-from .plotting import class_scatter, gaussian_mixture_plot
-from .training import all_checkpoint_paths, best_checkpoint_path, best_epoch
-from .utils import best_device
