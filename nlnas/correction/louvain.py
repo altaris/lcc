947edccd4c38fbb4ec5967185f3fe86831178b6d
Cédric HT <altaris@users.noreply.github.com>
2024-10-16 14:06:50 +0900

    Explicit cugraph dispatch

--- nlnas/correction/louvain.py
+++ nlnas/correction/louvain.py
@@ -1,5 +1,7 @@
 """Louvain clustering specific stuff"""
 
+import warnings
+from collections import defaultdict
 from typing import Any, Iterator, Literal
 
 import faiss
@@ -10,6 +12,37 @@ from torch import Tensor
 from torch.utils.data import DataLoader
 
 from ..utils import check_cuda, make_tqdm, to_array
+
+
+def _louvain_or_leiden(graph: nx.Graph, device: Any = None) -> list[set[int]]:
+    """
+    Explicit dispatch to
+    [`nx.community.louvain_communities`](https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.community.louvain.louvain_communities.html)
+    or
+    [`cugraph.leiden`](https://docs.rapids.ai/api/cugraph/stable/api_docs/api/cugraph/cugraph.leiden)
+    depending on the availability of CUDA and the requested device.
+
+    Args:
+        graph (nx.Graph):
+        device (Any, optional):
+    """
+    use_cuda, device = check_cuda(device)
+    if use_cuda:
+        import cugraph
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            # Note: since cugraph.leiden receives a networkx object, it returns
+            # tuple[dict[int, int], float]. This behavior isn't documented but
+            # the implementation is clear
+            # https://github.com/rapidsai/cugraph/blob/add69b8e2a63f4158172f78461f2f9703cbe8eaf/python/cugraph/cugraph/community/leiden.py#L126
+            ni_to_ci: dict[int, int] = cugraph.leiden(graph)[0]
+            # si = node index, ci = community index
+        ci_to_nis = defaultdict(list)
+        for ni, ci in ni_to_ci.items():
+            ci_to_nis[ci].append(ni)
+        return [set(v) for v in ci_to_nis.values()]
+    return nx.community.louvain_communities(graph)  # type: ignore
 
 
 def louvain_communities(
@@ -47,7 +80,7 @@ def louvain_communities(
            `y_louvain[i] == j`, then `z[i]` belongs to the $j$-th community
     """
 
-    use_cuda, device = check_cuda(device)
+    _, device = check_cuda(device)
 
     def _batches(desc: str | None = None) -> Iterator[Tensor]:  # shorthand
         if desc:
@@ -75,9 +108,9 @@ def louvain_communities(
         for j, all_i, all_d in zip(range(len(idx)), idx, dst):
             a = list(zip(all_i, all_d))[1:]  # exclude self as nearest neighbor
             graph.add_weighted_edges_from(
-                # [(n + j, int(i), np.exp(-d / np.sqrt(pca_dim))) for i, d in a]
-                # had numerical stability issues with above weights
                 [(n + j, int(i), 1) for i, _ in a]
+                # Had numerical stability issues with
+                # [(n + j, int(i), np.exp(-d / np.sqrt(pca_dim))) for i, d in a]
             )
         n += len(batch)
     graph.remove_edges_from(nx.selfloop_edges(graph))
@@ -86,10 +119,7 @@ def louvain_communities(
     # https://networkx.org/documentation/stable/reference/classes/generated/networkx.DiGraph.to_undirected.html
     graph = nx.to_undirected(graph)
 
-    communities: list[set[int]] = nx.community.louvain_communities(
-        graph,
-        **({"backend": "cugraph"} if use_cuda else {}),
-    )
+    communities = _louvain_or_leiden(graph, device)
     y_louvain = [0] * n
     for i, c in enumerate(communities):
         for n in c:
