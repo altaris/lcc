86d539d9ad4d59adb84e44f37b6ea4f5174482f2
Cédric HT <altaris@users.noreply.github.com>
2024-10-25 11:16:12 +0900

    Updated LCC scripts

--- lcc.sh
+++ lcc.sh
@@ -1,54 +1,97 @@
 #!/bin/sh
 
-# shellcheck disable=SC2046
-
 # https://www.patorjk.com/software/taag/
 # Font Name: ANSI Regular
 echo
-echo '██       ██████  ██████'
-echo '██      ██      ██'
-echo '██      ██      ██'
-echo '██      ██      ██'
-echo '███████  ██████  ██████'
+echo '██       ██████  ██████ '
+echo '██      ██      ██      '
+echo '██      ██      ██      '
+echo '██      ██      ██      '
+echo '███████  ██████  ██████ '
 
-# FILE=out/ft/cifar100/timm-mobilenetv3_small_050.lamb_in1k/results.0.json
-# LCC_SUBMODULES=classifier
+# DATASET="cifar100"
+# TRAIN_SPLIT='train[:80%]'
+# VAL_SPLIT='train[80%:]'
+# TEST_SPLIT="test"
+# IMAGE_KEY="img"
+# LABEL_KEY="fine_label"
 
-FILE=out/ft/cifar100/timm-tinynet_e.in1k/results.0.json
-LCC_SUBMODULES=classifier
+DATASET="ILSVRC/imagenet-1k"
+TRAIN_SPLIT='train[:80%]'
+VAL_SPLIT='train[80%:]'
+TEST_SPLIT="validation"
+IMAGE_KEY="image"
+LABEL_KEY="label"
 
-LCC_WEIGHT=1e-3
+# MODEL="google/mobilenet_v2_1.0_224"
+# HEAD_NAME="classifier"
+# LCC_SUBMODULES="classifier"
+
+# MODEL="google/vit-base-patch16-224"
+# HEAD_NAME="classifier"
+# LCC_SUBMODULES="classifier"
+
+# MODEL="microsoft/resnet-18"
+# HEAD_NAME="classifier.1"
+# LCC_SUBMODULES="classifier.1"
+
+MODEL="timm/resnet18.a3_in1k"
+HEAD_NAME="fc"
+LCC_SUBMODULES="fc"
+
+# MODEL="timm/mobilenetv3_small_050.lamb_in1k"
+# HEAD_NAME="classifier"
+# LCC_SUBMODULES="conv_head,classifier"
+
+# MODEL="timm/tinynet_e.in1k"
+# HEAD_NAME="classifier"
+# LCC_SUBMODULES="conv_head"
+
+LCC_WEIGHT=1e-4
 LCC_INTERVAL=1
 LCC_WARMUP=1
 CE_WEIGHT=1
 
-export CUDA_VISIBLE_DEVICES=0
+OUTPUT_DIR="out/sweep"
+export CUDA_VISIBLE_DEVICES=0,1
 
 echo
-echo "=================================================="
-echo "FT RESULT FILE: $FILE"
-echo "--------------------------------------------------"
+echo "======================================================================"
+echo "OUTPUT_DIR:     $OUTPUT_DIR"
+echo "Cuda devs:      $CUDA_VISIBLE_DEVICES"
+echo "----------------------------------------------------------------------"
+echo "MODEL:          $MODEL"
+echo "HEAD_NAME:      $HEAD_NAME"
+echo "----------------------------------------------------------------------"
 echo "CE_WEIGHT:      $CE_WEIGHT"
 echo "LCC_SUBMODULES: $LCC_SUBMODULES"
 echo "LCC_WEIGHT:     $LCC_WEIGHT"
 echo "LCC_INTERVAL:   $LCC_INTERVAL"
 echo "LCC_WARMUP:     $LCC_WARMUP"
-echo "=================================================="
+echo "----------------------------------------------------------------------"
+echo "DATASET:        $DATASET"
+echo "TRAIN_SPLIT:    $TRAIN_SPLIT"
+echo "VAL_SPLIT:      $VAL_SPLIT"
+echo "TEST_SPLIT:     $TEST_SPLIT"
+echo "IMAGE_KEY:      $IMAGE_KEY"
+echo "LABEL_KEY:      $LABEL_KEY"
+echo "======================================================================"
 echo
 
+# Don't specify --ckpt-path to start with weights from Hugging Face
 uv run python -m nlnas train \
-    $(jq -r .model.name < $FILE) \
-    $(jq -r .dataset.name < $FILE) \
-    out/lcc \
-    --lcc-submodules $LCC_SUBMODULES \
-    --lcc-weight $LCC_WEIGHT \
-    --lcc-interval $LCC_INTERVAL \
-    --lcc-warmup $LCC_WARMUP \
-    --ce-weight $CE_WEIGHT \
-    --ckpt-path out/ft/$(jq -r .fine_tuning.best_checkpoint.path < $FILE) \
-    --train-split $(jq -r .dataset.train_split < $FILE) \
-    --val-split $(jq -r .dataset.val_split < $FILE) \
-    --test-split $(jq -r .dataset.test_split < $FILE) \
-    --image-key $(jq -r .dataset.image_key < $FILE) \
-    --label-key $(jq -r .dataset.label_key < $FILE) \
-    --head-name $(jq -r .fine_tuning.hparams.head_name < $FILE)
+    "$MODEL" \
+    "$DATASET" \
+    "$OUTPUT_DIR" \
+    --ce-weight "$CE_WEIGHT" \
+    --lcc-submodules "$LCC_SUBMODULES" \
+    --lcc-weight "$LCC_WEIGHT" \
+    --lcc-interval "$LCC_INTERVAL" \
+    --batch-size 128 \
+    --lcc-warmup "$LCC_WARMUP" \
+    --train-split "$TRAIN_SPLIT" \
+    --val-split "$VAL_SPLIT" \
+    --test-split "$TEST_SPLIT" \
+    --image-key "$IMAGE_KEY" \
+    --label-key "$LABEL_KEY" \
+    --head-name "$HEAD_NAME"
