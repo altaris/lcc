078023f98d959f97944bcab5b971782303ecb7c6
Cédric HT <altaris@users.noreply.github.com>
2024-12-17 11:41:26 +0900

    Improved nuke script

--- nuke.py
+++ nuke.py
@@ -1,29 +1,37 @@
 """
-Kills every `/lcc/.venv/bin/python3` process listed by `nvidia-smi`, and then
-every `/lcc/.venv/bin/python3 -m lcc` process listed by `ps`.
+Kills every
+- `/lcc/.venv/bin/python3` process listed by `nvidia-smi`,
+- every `/lcc/.venv/bin/python3 -m lcc` process listed by `ps`.
+- every `pt_main_thread` process listed by `ps`.
 """
 
 import re
 import subprocess
+from getpass import getuser
+from time import sleep
+
+ART = """
+
+        ⣀⣠⣀⣀  ⣀⣤⣤⣄⡀
+   ⣀⣠⣤⣤⣾⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣶⣿⣿⣿⣶⣤⡀
+ ⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷
+ ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀
+ ⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿s⣿⣿⣿⣿⣿⣿⣿⣿⡇
+ ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁
+  ⠻⢿⡿⢿⣿⣿⣿⣿⠟⠛⠛⠋⣀⣀⠙⠻⠿⠿⠋⠻⢿⣿⣿⠟
+      ⠈⠉⣉⣠⣴⣷⣶⣿⣿⣿⣿⣶⣶⣶⣾⣶
+        ⠉⠛⠋⠈⠛⠿⠟⠉⠻⠿⠋⠉⠛⠁
+              ⣶⣷⡆
+      ⢀⣀⣠⣤⣤⣤⣤⣶⣿⣿⣷⣦⣤⣤⣤⣤⣀⣀
+    ⢰⣿⠛⠉⠉⠁   ⢸⣿⣿⣧    ⠉⠉⠙⢻⣷
+     ⠙⠻⠷⠶⣶⣤⣤⣤⣿⣿⣿⣿⣦⣤⣤⣴⡶⠶⠟⠛⠁
+          ⢀⣴⣿⣿⣿⣿⣿⣿⣷⣄
+         ⠒⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠓
+
+"""
 
 if __name__ == "__main__":
-    print()
-    print("        ⣀⣠⣀⣀  ⣀⣤⣤⣄⡀           ")
-    print("   ⣀⣠⣤⣤⣾⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣶⣿⣿⣿⣶⣤⡀    ")
-    print(" ⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷    ")
-    print(" ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀ ")
-    print(" ⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ ")
-    print(" ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁ ")
-    print("  ⠻⢿⡿⢿⣿⣿⣿⣿⠟⠛⠛⠋⣀⣀⠙⠻⠿⠿⠋⠻⢿⣿⣿⠟    ")
-    print("      ⠈⠉⣉⣠⣴⣷⣶⣿⣿⣿⣿⣶⣶⣶⣾⣶        ")
-    print("        ⠉⠛⠋⠈⠛⠿⠟⠉⠻⠿⠋⠉⠛⠁        ")
-    print("              ⣶⣷⡆             ")
-    print("      ⢀⣀⣠⣤⣤⣤⣤⣶⣿⣿⣷⣦⣤⣤⣤⣤⣀⣀      ")
-    print("    ⢰⣿⠛⠉⠉⠁   ⢸⣿⣿⣧    ⠉⠉⠙⢻⣷    ")
-    print("     ⠙⠻⠷⠶⣶⣤⣤⣤⣿⣿⣿⣿⣦⣤⣤⣴⡶⠶⠟⠛⠁    ")
-    print("          ⢀⣴⣿⣿⣿⣿⣿⣿⣷⣄          ")
-    print("         ⠒⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠓         ")
-    print()
+    print(ART)
 
     raw = subprocess.check_output(["nvidia-smi"])
     pids = []
@@ -31,22 +39,48 @@ if __name__ == "__main__":
     for line in raw.decode("utf-8").split("\n"):
         if m := re.search(r, line):
             pids.append(m.group(1))
-
     if not pids:
         print("[nvidia-smi] No matching processes found.")
     else:
-        print("[nvidia-smi] Nuking processes:", pids)
-        subprocess.run(["kill", "-9"] + pids, check=True)
+        print("[nvidia-smi] Nuking processes:", " ".join(pids))
+        try:
+            subprocess.run(["kill", "-9"] + pids, check=True)
+        except Exception as e:
+            print("Nuke didn't detonate:", e)
 
-    raw = subprocess.check_output(["ps", "-u", "cedric", "-eo", "pid,comm"])
+    print("Sleeping for 1 second")
+    sleep(1)
+
+    USER = getuser()
+    raw = subprocess.check_output(["ps", "-u", USER, "-eo", "pid,comm"])
     pids = []
     r = re.compile(r"(\d+) .*/lcc/\.venv/bin/python3.*m lcc.*")
     for line in raw.decode("utf-8").split("\n"):
         if m := re.search(r, line):
             pids.append(m.group(1))
-
     if not pids:
-        print("[ps] No matching processes found.")
+        print("[ps python3] No matching processes found.")
     else:
-        print("[ps] Nuking processes:", pids)
-        subprocess.run(["kill", "-9"] + pids, check=True)
+        print("[ps python3] Nuking processes:", " ".join(pids))
+        try:
+            subprocess.run(["kill", "-9"] + pids, check=True)
+        except Exception as e:
+            print("Nuke didn't detonate:", e)
+
+    print("Sleeping for 1 second")
+    sleep(1)
+
+    raw = subprocess.check_output(["ps", "-u", USER, "-eo", "pid,comm"])
+    pids = []
+    r = re.compile(r"(\d+) pt_main_thread")
+    for line in raw.decode("utf-8").split("\n"):
+        if m := re.search(r, line):
+            pids.append(m.group(1))
+    if not pids:
+        print("[ps pt_main_thread] No matching processes found.")
+    else:
+        print("[ps pt_main_thread] Nuking processes:", " ".join(pids))
+        try:
+            subprocess.run(["kill", "-9"] + pids, check=True)
+        except Exception as e:
+            print("Nuke didn't detonate:", e)
